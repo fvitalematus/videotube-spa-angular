92a7c516926a0af51af4ba9dada07140bad64868
Franco Vitale <franco.vitalematus@gmail.com>
2020-08-03 19:46:31 -0400

    Ajustes y Edición de Datos: Form, Edición y Actualización en la BD

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -49,6 +49,17 @@ export class UserService {
         return this._http.post(this.url + 'login', params, { headers: headers });
     }
 
+    update(token, user): Observable<any> {
+        
+        let json = JSON.stringify(user);
+        let params = 'json=' + json;
+
+        let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+            .set('Authorization', token);
+
+        return this._http.put(this.url + 'user/edit', params, { headers: headers });
+    }
+
     getIdentity() {
         let identity = JSON.parse(localStorage.getItem('identity'));
 
